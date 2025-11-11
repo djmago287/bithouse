e947be7f7e16d2bcc6ff54cce2d654ebfd165de9
BITCAS <casaminmaicol8@gmail.com>
2025-11-11 18:10:46 -0500

    url request

--- src/infrastructure/request_getbackup.jsx
+++ src/infrastructure/request_getbackup.jsx
@@ -1,5 +1,5 @@
 export const Request_getallbackupmysqlfiles  =  async ()=>{
-    const url = 'http://192.168.192.158:5173//backupdb';
+    const url = 'http://192.168.100.12:5173/backupdb';
     const request = await fetch(url);
     const response =  await request.json();
     return response;
